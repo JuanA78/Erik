a7d5cc7c0251c72b719a90c1e515c61868791b8a
Juan Angel <juanc199pk1516@gmail.com>
2025-05-20 00:53:10 -0600

    nuevosCambios4

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
+require('dotenv').config();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a la base de datos MySQL');
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
 });
 
-module.exports = connection;
+module.exports = db;
