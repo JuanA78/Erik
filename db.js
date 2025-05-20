d04bab662fab676f19d102927da1f769001071b9
Juan Angel <juanc199pk1516@gmail.com>
2025-05-20 00:12:54 -0600

    Actualización del proyecto

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'sql5.freesqldatabase.com',
-  user: 'sql5779898',
-  password: 'RXj93PXjj1',
-  database: 'sql5779898',
-  port:3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
